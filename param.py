10b8547a9af94ddf0f1731f395a9b0ec4b059449
Jamie Tan <tanjt107@gmail.com>
2022-03-04 12:58:32 +0800

    New branch for archive files

---
+++ param.py
@@ -0,0 +1,42 @@
+sheet_name = "FOODPIGPI 二月第二團～ (Responses)"
+responses_sheet_name = "Form responses 1"
+payment_deadline = "14/2"
+bulk_list_dict = [
+    # {"type": "Category", "item": "👉🏼雪Q餅", "quantity": 6, "discount": 4},
+    # {"type": "Category", "item": "👉🏼雪Q餅", "quantity": 12, "discount": 10},
+    # {"type": "Category", "item": "👉🏼貝果🥯 (買5顆可減$5)", "quantity": 5, "discount": 5},
+]
+
+message_template = """👉🏻FOODPIGPI 貝果訂單確認單🥯
+Hello🙋🏻‍♀️多謝你支持😆🤍🤍
+你訂了
+{}
+🏃🏽交收時間係🏃🏽
+{}
+👉🏻TOTAL : {}
+👉🏻⭐⭐確認訂單後 麻煩必須回傳信息確認⭐⭐
+👉🏻此為最後確認單⚠️
+*如msg內沒有列出的項目表示滿了團啦～*
+
+付款方法💰
+📝必須⚠️ 回 傳 付 款 證 明 ⚠️
+📝必須在備註寫下自己ig name
+
+💥 PAYME已爆 ‼️
+1️⃣ 轉數快 / Alipay (6433 4035)
+KWAN Sxx Yx
+2️⃣️⃣ 恆生櫃員機轉帳
+777-222241-882
+KWAN Sxx Yx
+3️⃣ ‼️‼️ 消費卷 ‼️‼️
+門市 - TapnGo
+QR CODE - Alipay / WeChat Pay
+
+✊🏻🌟請在 {} 或之前入數 有咩需要就同我講啦🌟
+1️⃣🙏🏻如沒有在限期前入數會自動取消訂單的 所以盡量即日入數 😳🤎
+2️⃣🙏🏻發出訂單通知24小時後不得作任何更改 
+3️⃣🙏🏻如截數前24小時內棄單 會列入黑名單
+4️⃣🙏🏻當日臨時更改交收方式地點將收取$𝟙𝟘手續費🙏🏻
+🌟交收當天早上會發出交收提醒 如沒有收到麻煩提醒一下🙏🏻🙏🏻
+👉🏻所有面交點 逾 時 不 候 如交收突然遲大到/唔見人 會被視為自動棄單並不會退款🙏🏻
+📦冷凍速遞會於星期六寄出 星期日收件"""
